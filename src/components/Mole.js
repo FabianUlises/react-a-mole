f67ea9e7f9a67cfe030607e685d8a2df438d035d
FabianUlises <ulisesfab.car@outlook.com>
2023-03-31 11:23:02 -0700

    Updated timer

--- src/components/Mole.js
+++ src/components/Mole.js
@@ -4,14 +4,14 @@ const Mole = (props) => {
   // Useeffect to toggle theMole prop from props
   useEffect(() => {
     // Generating random number
-    let randSeconds = Math.ceil(Math.random() * 10000);
+    let randSeconds = Math.ceil(Math.random() * 2500);
     // Timer to toggle mole
     let timer = setTimeout(() => {
         props.toggle(false)
     }, randSeconds)
     // Resetting timer
     return () => clearTimeout(timer)
-});
+}, []);
   return (
     <div>
       <img style={{'width': '100%'}} src={moleImg} alt='mole' onClick={props.handleClick} />
